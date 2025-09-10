7d58cc49efedfe9b41032b5a116970cfb18175b9
ucmayy <chinmaymahto1905@gmail.com>
2025-09-10 22:28:25 +0530

    added features and login page

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Features from "./pages/Features";
+import Footer from "./pages/Footer";
+import LoginPage from "./pages/LoginPage";
+
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen bg-white text-gray-900">
+  
+      <Features />
+      {/* <Footer />
+      <LoginPage /> */}
+
+    </div>
+  );
+}
+
+export default App;
