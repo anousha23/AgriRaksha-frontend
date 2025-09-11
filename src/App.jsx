ccd4e7ab6687d8d0d41918162f2ab76628e5eae1
anousha23 <ambar.anousha@gmail.com>
2025-09-11 05:30:29 +0530

    Added HeroSection

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import HeroSection from "./HeroSection";
+
+const App = () => {
+  return (
+    <div className="flex flex-col min-h-screen bg-white text-gray-900">
+ 
+         <HeroSection />
+    </div>
+  );
+};
+
+export default App;
